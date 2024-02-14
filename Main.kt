0f29c06e6257b28dc1438e77b67b27a7f06aabdf
Izieiurov Stepan <stepan123512@gmail.com>
2024-02-14 11:01:05 +0300

    1-st commit - menu list

--- Main.kt
+++ Main.kt
@@ -1,3 +1,25 @@
 fun main() {
+    clearConsole()
     println("Hello, kotlin!")
+    val list = arrayOf(
+        "Программы",
+        "Прог1",
+        "Прог2",
+        "Прог3"
+    )
+    val list_quantity: Int = list.size;
+
+
+    for (i in 0..list_quantity)
+    {
+        println("[" + i + "] " + list[i])
+    }
+
 }
+
+/* kotlinc src/Main.kt -include-runtime -d out/main.jar
+java -jar out/main.jar */
+
+fun clearConsole() {
+        repeat(100) { println() }
+    }
